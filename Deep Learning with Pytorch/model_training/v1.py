6dbd252167ac47bc42045697399d6c0db09cb015
Anushka <anushkachathuranga1998@gmail.com>
2024-01-31 20:11:59 +0530

    train config

---
+++ Deep Learning with Pytorch/model_training/v1.py
@@ -0,0 +1,8 @@
+n_epochs=10000
+losses=[]
+
+for epoch in tqdm(range(n_epochs)):
+
+    # perform train step and return corresponding loss
+    loss=train_step_fn(X_train_tensor,y_train_tensor) # perform one training step
+    losses.append(loss) # keep track of loss
