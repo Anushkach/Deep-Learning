3700917fa1132a95694fff53297b27318797809b
Anushka <anushkachathuranga1998@gmail.com>
2024-02-02 21:54:54 +0530

    training (MODIFIEED)

---
+++ Deep Learning with Pytorch/data_preparation/v1.py
@@ -0,0 +1,9 @@
+
+X_train_tensor=torch.as_tensor(X_train).float()
+y_train_tensor=torch.as_tensor(y_train).float()
+
+# Builds Dataset
+train_data=TensorDataset(X_train_tensor,y_train_tensor)
+
+# Build Dataloader
+train_loader=DataLoader(dataset=train_data,batch_size=16,shuffle=True)
