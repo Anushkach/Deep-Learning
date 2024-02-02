8a5b81e167129437cc5805d892b407084b25df78
Anushka <anushkachathuranga1998@gmail.com>
2024-02-03 02:18:17 +0530

    minor changes

--- Deep Learning with Pytorch/data_preparation/v2.py
+++ Deep Learning with Pytorch/data_preparation/v2.py
@@ -1,21 +1,21 @@
 
-torch.manual_seed(42)
+torch.manual_seed(13)
 
-# Build tensors from np arrays
-X_tensor=torch.as_tensor(x).float()
-y_tensor=torch.as_tensor(y).float()
+# Builds tensors from numpy arrays BEFORE split
+x_tensor = torch.from_numpy(x).float()
+y_tensor = torch.from_numpy(y).float()
 
-# build dataset containing all data
-dataset=TensorDataset(X_tensor,y_tensor)
+# Builds dataset containing ALL data points
+dataset = TensorDataset(x_tensor, y_tensor)
 
-# perform split
-split_ratio=0.8
-n_total=len(dataset)
-n_train=int(n_total*split_ratio)
-n_val=n_total-n_train
+# Performs the split
+ratio = .8
+n_total = len(dataset)
+n_train = int(n_total * ratio)
+n_val = n_total - n_train
 
-# perform train-test split in pytorch
-train_data,val_data=random_split(dataset,[n_train,n_val])
+train_data, val_data = random_split(dataset, [n_train, n_val])
 
-train_loader=DataLoader(dataset=train_data,batch_size=16,shuffle=True)
-val_loader=DataLoader(dataset=val_data,batch_size=16) # we dont use shuffle for validation data because we want to keep the order
+# Builds a loader of each set
+train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=16)
